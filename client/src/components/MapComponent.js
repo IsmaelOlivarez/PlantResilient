001cb96b13bcb2f42c131f5f8ba2c2a9e09a133e
Ismael Olivarez <ismael.olivarez05@gmail.com>
2025-02-09 09:54:11 -0500

    Color gradient feature implemented, must implement feature that changes the timeline and polution

--- client/src/components/MapComponent.js
+++ client/src/components/MapComponent.js
@@ -35,9 +35,35 @@ const MapComponent = () => {
   const [geojsonData, setGeojsonData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [zoomLevel, setZoomLevel] = useState(7);
+  const [reloadKey, setReloadKey] = useState(0); // Add a reload key to trigger re-render
+
+  const mode = 2;
+  const polution = true;
+
+  const hardinessMap = {
+    "5b": "#5ec9e0",
+    "6a": "#56bb48",
+    "6b": "#78c756",
+    "7a": "#abd669",
+    "7b": "#cddb70",
+    "8a": "#eeda85",
+    "8b": "#ebcb57",
+    "9a": "#dbb64f",
+    "9b": "#f5b678",
+    "10a": "#eb9d3d",
+    "10b": "#e67937",
+    "11a": "#e65733",
+    "11b": "#e88564",
+    "12a": "#d4594f",
+    "12b": "#b62929"
+
+  };
 
   const handleToggle = (isToggled) => {
     setZoomLevel(isToggled ? 10 : 7);  // Toggle between zoom levels 7 and 10
+
+    // Change the reloadKey to force a re-render and reload the colors
+    setReloadKey(prevKey => prevKey + 1);
   };
 
   // Fetch GeoJSON data from the server
@@ -57,7 +83,78 @@ const MapComponent = () => {
     fetchGeojsonData();
   }, []);  
 
-  const onEachFeature = (feature, layer) => {
+  const getStyle = (feature) => {
+    // Default color
+    const color = 'gray';
+    
+    return {
+      color: 'red', // Border color
+      weight: 0,
+      opacity: 0.0,
+      fillColor: color, // Default fill color (we'll update this asynchronously)
+      fillOpacity: 0.7
+    };
+  };
+
+  const onEachFeature = async (feature, layer) => {
+    const zipCode = feature.properties.ZCTA5CE10;
+    let color = 'gray';  // Default color in case of failure
+
+    try {
+      let response;
+      if(polution){
+        switch(mode) {
+          case 0:
+            response = await fetch(`/api/earlyHigh/${zipCode}`);
+            break;
+          case 1:
+            response = await fetch(`/api/midHigh/${zipCode}`);
+            break;
+          case 2:
+            response = await fetch(`/api/lateHigh/${zipCode}`);
+            break;
+          default:
+            mode = 0
+            response = await fetch(`/api/earlyHigh/${zipCode}`);
+        }
+      }else{
+        switch(mode) {
+          case 0:
+            response = await fetch(`/api/earlyLow/${zipCode}`);
+            break;
+          case 1:
+            response = await fetch(`/api/midLow/${zipCode}`);
+            break;
+          case 2:
+            response = await fetch(`/api/lateLow/${zipCode}`);
+            break;
+          default:
+            mode = 0
+            response = await fetch(`/api/earlyLow/${zipCode}`);
+
+        }
+      }
+      
+      if (response.ok) {
+        const data = await response.json();
+        const zone = data.zone;
+        console.log("Fetched Zone:", zone);
+        color = hardinessMap[zone] || 'gray'; // Use the corresponding color for the zone
+      }
+    } catch (error) {
+      console.error("Error fetching zone data:", error);
+    }
+
+    // Now, apply the dynamically fetched color using setStyle
+    layer.setStyle({
+      color: color, // Border color
+      weight: 1,
+      opacity: 0.7,
+      fillColor: color,  // Set dynamic fill color
+      fillOpacity: 0.7
+    });
+
+    // Event listeners for mouse interactions
     layer.on({
       mouseover: (e) => {
         const layer = e.target;
@@ -69,36 +166,28 @@ const MapComponent = () => {
         });
       },
       mouseout: (e) => {
-        const layer = e.target;
+        // Reapply the dynamic style when mouse leaves
         layer.setStyle({
+          color: color, // Border color
           weight: 1,
-          color: 'blue',
-          fillColor: 'gray',
-          fillOpacity: 0.2,
+          opacity: 0.7,
+          fillColor: color, // Reapply the fetched color
+          fillOpacity: 0.7
         });
       },
       click: (e) => {
         alert(`You clicked on zip code: ${feature.properties.ZCTA5CE10}`);
-      },
+      }
     });
 
-    // Optionally, bind popup or tooltip with data
+    // Optionally, bind a popup with the zip code
     layer.bindPopup(`Zip Code: ${feature.properties.ZCTA5CE10}`);
-  };
-
-  const geoJsonStyle = {
-    color: 'blue', 
-    weight: 1,
-    opacity: 0.7, 
-    fillColor: 'gray',
-    fillOpacity: .2
   };
 
   return (
     <div>
       <section style={{ position: 'absolute', width: '100%' }}>
         <div style={{ height: '750px', width: '750px', position: 'relative', margin: '0 auto', marginLeft: 80 }}>
-
           {/* Loading Spinner */}
           {isLoading && (
             <div className="loading-spinner">
@@ -115,10 +204,16 @@ const MapComponent = () => {
             whenReady={() => setIsLoading(false)} // Set loading to false when map is ready
           >
             <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            {geojsonData && <GeoJSON data={geojsonData} onEachFeature={onEachFeature} style={geoJsonStyle} />}
+            {geojsonData && (
+              <GeoJSON 
+                data={geojsonData} 
+                onEachFeature={onEachFeature} 
+                style={getStyle} 
+                key={reloadKey} // Add the reloadKey here
+              />
+            )}
             <MapUpdater zoom={zoomLevel} />
           </MapContainer>
-
         </div>
         <ToggleSwitch onToggle={handleToggle} />
       </section>
--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const fs = require('fs');
+const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config();
+console.log('Mongo URI:', process.env.MONGODB_URI);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,38 +16,212 @@ app.use(cors());
 // Serve static files (React build output)
 app.use(express.static(path.join(__dirname, '../client/build'))); // Adjusted to serve from the client build directory
 
+const mongoURI = process.env.MONGODB_URI;
+
+
+// Connect to MongoDB using Mongoose
+mongoose.connect('mongodb+srv://ismaelolivarez05:vURxIlNbjL8gHyV6@plantresilient1.hqmlc.mongodb.net/zipZones?retryWrites=true&w=majority&appName=plantresilient1', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log('MongoDB connected successfully'))
+    .catch((err) => console.log('MongoDB connection error: ', err));
+
+// Example route to check if server is running
+app.get('/', (req, res) => {
+    res.send('Hello, MongoDB!');
+});
+
+const hardinessSchema = new mongoose.Schema({
+    zipCode: { type: String, required: true },
+    zone: { type: String, required: true }
+});
+
+const zoneSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+  });
+  
+  const Zone = mongoose.model('Zone', zoneSchema);
+
+const earlyLowSchema = new mongoose.Schema({
+    zipcode: { type: String, required: true },
+    zone: { type: String, required: true }
+}, { collection: 'earlyLow' }); // Specify the collection name
+const earlyHighSchema = new mongoose.Schema({
+    zipcode: { type: String, required: true },
+    zone: { type: String, required: true }
+}, { collection: 'earlyHigh' }); // Specify the collection name
+const midLowSchema = new mongoose.Schema({
+    zipcode: { type: String, required: true },
+    zone: { type: String, required: true }
+}, { collection: 'midLow' }); // Specify the collection name
+const midHighSchema = new mongoose.Schema({
+    zipcode: { type: String, required: true },
+    zone: { type: String, required: true }
+}, { collection: 'midHigh' }); // Specify the collection name
+const lateLowSchema = new mongoose.Schema({
+    zipcode: { type: String, required: true },
+    zone: { type: String, required: true }
+}, { collection: 'lateLow' }); // Specify the collection name
+const lateHighSchema = new mongoose.Schema({
+    zipcode: { type: String, required: true },
+    zone: { type: String, required: true }
+}, { collection: 'lateHigh' }); // Specify the collection name
+
+const EarlyLow = mongoose.model('EarlyLow', earlyLowSchema); // Model for the 'earlyHigh' collection
+const EarlyHigh = mongoose.model('EarlyHigh', earlyHighSchema); // Model for the 'earlyHigh' collection
+const MidLow = mongoose.model('MidLow', midLowSchema); // Model for the 'earlyHigh' collection
+const MidHigh = mongoose.model('MidHigh', midHighSchema); // Model for the 'earlyHigh' collection
+const LateLow = mongoose.model('LateLow', lateLowSchema); // Model for the 'earlyHigh' collection
+const LateHigh = mongoose.model('LateHigh', lateHighSchema); // Model for the 'earlyHigh' collection
+
+
+
+
 // API endpoint to serve GeoJSON data
 app.get('/api/geojson-data', (req, res) => {
-  // The GeoJSON files are now located in the /geojson folder
-  const geojsonFiles = [
-    'fl_florida_zip_codes_geo.min.json',
-    'sc_south_carolina_zip_codes_geo.min.json',
-    'ga_georgia_zip_codes_geo.min.json',
-    'ms_mississippi_zip_codes_geo.min.json',
-    'al_alabama_zip_codes_geo.min.json'
-  ];
+    // The GeoJSON files are now located in the /geojson folder
+    const geojsonFiles = [
+        'fl_florida_zip_codes_geo.min.json',
+        'sc_south_carolina_zip_codes_geo.min.json',
+        'ga_georgia_zip_codes_geo.min.json',
+        'ms_mississippi_zip_codes_geo.min.json',
+        'al_alabama_zip_codes_geo.min.json'
+    ];
 
-  // Read all GeoJSON files from the geojson folder
-  const geojsonData = geojsonFiles.map(filePath => {
-    // Correctly resolve the file path to the geojson folder
-    const absolutePath = path.join(__dirname, '../geojson', filePath); // Adjusted path to point to /geojson folder
-    return fs.readFileSync(absolutePath, 'utf8'); // Read file synchronously
-  });
 
-  // Combine the GeoJSON files into a single array of objects
-  const combinedGeojson = geojsonData.map(data => JSON.parse(data));
+    // Read all GeoJSON files from the geojson folder
+    const geojsonData = geojsonFiles.map(filePath => {
+        // Correctly resolve the file path to the geojson folder
+        const absolutePath = path.join(__dirname, '../geojson', filePath); // Adjusted path to point to /geojson folder
+        return fs.readFileSync(absolutePath, 'utf8'); // Read file synchronously
+    });
 
-  // Send the combined GeoJSON data to the client
-  res.json(combinedGeojson);
+    // Combine the GeoJSON files into a single array of objects
+    const combinedGeojson = geojsonData.map(data => JSON.parse(data));
+
+    // Send the combined GeoJSON data to the client
+    res.json(combinedGeojson);
 });
+app.get('/api/earlyLow/:zipcode', async (req, res) => { // CHANGE
+    const { zipcode } = req.params;  // Get the zipcode from params
+    const zipCodeString = String(zipcode);  // Ensure it's treated as a string
+    
+    try {
+        // Query the 'earlyHigh' collection using the 'EarlyHigh' model
+        const result = await EarlyLow.findOne({ zipcode: zipCodeString }); // CHANGE
+
+        if (!result) {
+            return res.status(404).json({ message: 'Zipcode not found' });
+        }
+
+        res.json({ zone: result.zone });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+app.get('/api/earlyHigh/:zipcode', async (req, res) => { // CHANGE
+    const { zipcode } = req.params;  // Get the zipcode from params
+    const zipCodeString = String(zipcode);  // Ensure it's treated as a string
+    
+    try {
+        // Query the 'earlyHigh' collection using the 'EarlyHigh' model
+        const result = await EarlyHigh.findOne({ zipcode: zipCodeString }); // CHANGE
+
+        if (!result) {
+            return res.status(404).json({ message: 'Zipcode not found' });
+        }
+
+        res.json({ zone: result.zone });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+app.get('/api/midLow/:zipcode', async (req, res) => { // CHANGE
+    const { zipcode } = req.params;  // Get the zipcode from params
+    const zipCodeString = String(zipcode);  // Ensure it's treated as a string
+    
+    try {
+        // Query the 'earlyHigh' collection using the 'EarlyHigh' model
+        const result = await MidLow.findOne({ zipcode: zipCodeString }); // CHANGE
+
+        if (!result) {
+            return res.status(404).json({ message: 'Zipcode not found' });
+        }
+
+        res.json({ zone: result.zone });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+app.get('/api/midHigh/:zipcode', async (req, res) => { // CHANGE
+    const { zipcode } = req.params;  // Get the zipcode from params
+    const zipCodeString = String(zipcode);  // Ensure it's treated as a string
+    
+    try {
+        // Query the 'earlyHigh' collection using the 'EarlyHigh' model
+        const result = await MidHigh.findOne({ zipcode: zipCodeString }); // CHANGE
+
+        if (!result) {
+            return res.status(404).json({ message: 'Zipcode not found' });
+        }
+
+        res.json({ zone: result.zone });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+app.get('/api/lateLow/:zipcode', async (req, res) => { // CHANGE
+    const { zipcode } = req.params;  // Get the zipcode from params
+    const zipCodeString = String(zipcode);  // Ensure it's treated as a string
+    
+    try {
+        // Query the 'earlyHigh' collection using the 'EarlyHigh' model
+        const result = await LateLow.findOne({ zipcode: zipCodeString }); // CHANGE
+        if (!result) {
+            return res.status(404).json({ message: 'Zipcode not found' });
+        }
+
+        res.json({ zone: result.zone });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+app.get('/api/lateHigh/:zipcode', async (req, res) => { // CHANGE
+    const { zipcode } = req.params;  // Get the zipcode from params
+    const zipCodeString = String(zipcode);  // Ensure it's treated as a string
+    
+    try {
+        // Query the 'earlyHigh' collection using the 'EarlyHigh' model
+        const result = await LateHigh.findOne({ zipcode: zipCodeString }); // CHANGE
+        if (!result) {
+            return res.status(404).json({ message: 'Zipcode not found' });
+        }
+
+        res.json({ zone: result.zone });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+
 
 // Fallback route for React app (SPA)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));  // Serve React app
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));  // Serve React app
 });
 
 
+
+  
+
+
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server is running on http://0.0.0.0:${PORT}`);
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
 
